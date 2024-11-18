9060ba7c2f452c10abaf3676da0d11022e138465
RabbitMimi <kitto.ringo@gmail.com>
2024-11-18 21:51:21 +0900

    js実装

--- main.js
+++ main.js
@@ -9,6 +9,10 @@
     todos = JSON.parse(localStorage.getItem("todos"));
   }
 
+  const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
   const renderTodo = (todo) => {
     /*
     -li
@@ -21,7 +25,14 @@
     const input = document.createElement("input");
     input.type = "checkbox";
     input.checked = todo.isCompleted;
-
+    input.addEventListener("change", () => {
+      todos.forEach((item) => {
+        if (item.id === todo.id) {
+          item.isCompleted = !item.isCompleted;
+        }
+      });
+      saveTodos();
+    });
     const span = document.createElement("span");
     span.textContent = todo.title;
     const label = document.createElement("label");
@@ -35,6 +46,10 @@
         return;
       }
       li.remove();
+      todos = todos.filter((item) => {
+        return item.id !== todo.id;
+      });
+      saveTodos();
     });
     const li = document.createElement("li");
     li.appendChild(label);
@@ -53,14 +68,30 @@
     e.preventDefault();
     const input = document.querySelector("#add-form input");
     const todo = {
+      id: Date.now(),
       title: input.value,
       isCompleted: false,
     };
     renderTodo(todo);
     todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos));
+    console.table(todos);
+    saveTodos();
     input.value = "";
-    input.focus;
+    input.focus();
+  });
+
+  document.querySelector("#purge").addEventListener("click", () => {
+    if (!confirm("Sure?")) {
+      return;
+    }
+    todos = todos.filter((todo) => {
+      return todo.isCompleted === false;
+    });
+    saveTodos();
+    document.querySelectorAll("#todos li").forEach((li) => {
+      li.remove();
+    });
+    renderTodos();
   });
 
   renderTodos();
